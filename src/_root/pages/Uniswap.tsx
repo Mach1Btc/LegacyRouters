8f17ee04dbbf14d8326cff9f21611556c1eaf530
Mach1Btc <allxcaps@gmail.com>
2025-06-11 23:07:25 -0500

    made layout more dynamic

--- src/_root/pages/Uniswap.tsx
+++ src/_root/pages/Uniswap.tsx
@@ -1,10 +1,9 @@
 import { UniswapSwapPanel } from "@/components/shared"
 
-
 const Uniswap = () => {
     return (
         <div className="flex flex-col items-center w-full h-full overflow-y-auto">
-            <div className="flex flex-col w-4/5 xl:w-3/4 h-full gap-4 items-center mt-[40px] sm:mt-[100px] md:mt-[180px] xl:mb-0">
+            <div className="flex flex-col w-4/5 xl:w-3/4 h-full gap-4 items-center justify-center">
                 <UniswapSwapPanel />
             </div>
         </div>
